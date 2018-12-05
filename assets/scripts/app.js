d9cc4edffe0d82a86a846d395d77c4482ad21414
Sergey Glazov <yaglazov@gmail.com>
2018-12-05 20:23:40 +0300

    Update 2018-12-05T17:23:37.552Z

---
+++ assets/scripts/app.js
@@ -0,0 +1,1 @@
+"use strict";$&&$(function(){$("a").hover(function(){""!=$(this).attr("href")&&"#"!=$(this).attr("href")&&$('a[href="'+$(this).attr("href")+'"]').addClass("hover")},function(){$("a").removeClass("hover")})});
